e6c23b2e6afbe4beda31c0bb2786a7d967e16c65
dmitriy.pilipenko <dmitriy.pilipenko@saludamedical.com>
2021-03-12 23:16:15 +0300

    Add slider content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useRef, useState} from 'react';
-import aituBridge from '@btsd/aitu-bridge';
+import { useEffect, useRef, useState } from "react";
+import aituBridge from "@btsd/aitu-bridge";
 import {
   IonApp,
   IonSlides,
@@ -7,28 +7,63 @@ import {
   IonContent,
   IonButton,
   IonText,
-} from '@ionic/react';
+} from "@ionic/react";
 
-import './App.css';
+import "./App.css";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
+
+interface ISlideContentProps {
+  title: string;
+  onClick: () => void;
+  description: string;
+  buttonTitle: string;
+  imgSrc: string;
+}
+
+const SlideContent: React.FC<ISlideContentProps> = ({
+  onClick,
+  title,
+  description,
+  buttonTitle,
+  imgSrc,
+}) => {
+  return (
+    <>
+      <img src={imgSrc} />
+      <div className="slide-block">
+        <IonText color="dark">
+          <h2>{title}</h2>
+        </IonText>
+        <IonText>
+          <sub>{description}</sub>
+        </IonText>
+      </div>
+      <div className="slide-button">
+        <IonButton expand="full" onClick={onClick}>
+          {buttonTitle}
+        </IonButton>
+      </div>
+    </>
+  );
+};
 
 const App: React.FC = () => {
   // Optional parameters to pass to the swiper instance.
@@ -36,8 +71,8 @@ const App: React.FC = () => {
   const slideOpts = {
     initialSlide: 0,
     speed: 400,
-  }
-  const slider = useRef<HTMLIonSlidesElement>(null)
+  };
+  const slider = useRef<HTMLIonSlidesElement>(null);
 
   async function getMe() {
     try {
@@ -47,7 +82,7 @@ const App: React.FC = () => {
       // handle error
       console.log(e);
     }
-  };
+  }
 
   useEffect(() => {
     if (aituBridge.isSupported()) {
@@ -55,76 +90,59 @@ const App: React.FC = () => {
     }
   }, []);
 
-  const [name, setName] = useState('<username>');
+  const [name, setName] = useState("<username>");
 
   const handleButtonClick = () => {
     slider.current?.slideNext();
-    console.log('nextSlide');
-  }
+  };
 
   return (
     <IonApp>
       <IonContent>
-        <IonSlides pager={true} options={slideOpts} ref={slider} >
+        <IonSlides pager={true} options={slideOpts} ref={slider}>
           <IonSlide>
-            <img src="/assets/slide1.png" />
-            <div className="slide-block">
-              <IonText color="dark">
-                <h2>Мини-приложения в Aitu</h2>
-              </IonText>
-              <IonText> <sub>Расскажем, что это и как использовать aitu.apps для своего бизнеса</sub></IonText>
-            </div>
-            <div className="slide-button">
-              <IonButton expand="full" onClick={handleButtonClick}>Я готов!</IonButton>
-            </div>
+            <SlideContent
+              title={`Привет, ${name}, Мини-приложения в Aitu`}
+              onClick={handleButtonClick}
+              description={
+                "Расскажем, что это и как использовать aitu.apps для своего бизнеса"
+              }
+              buttonTitle={"Я готов!"}
+              imgSrc={"/assets/slide1.png"}
+            ></SlideContent>
           </IonSlide>
           <IonSlide>
-            <img src="/assets/slide2.png" />
-            <div className="slide-block">
-              <IonText color="dark">
-                <h2>+800.000 пользователей Aitu</h2>
-              </IonText>
-              <IonText> <sub>Могут увидеть ваше мини-приложение и стать его пользователями</sub></IonText>
-            </div>
-            <div className="slide-button">
-              <IonButton expand="full" onClick={handleButtonClick}>Интересно</IonButton>
-            </div>
+            <SlideContent
+                title={"+800.000 пользователей Aitu"}
+                onClick={handleButtonClick}
+                description={
+                  "Могут увидеть ваше мини-приложение и стать его пользователями"
+                }
+                buttonTitle={"Интересно"}
+                imgSrc={"/assets/slide2.png"}
+            ></SlideContent>
           </IonSlide>
           <IonSlide>
-            <img src="/assets/slide3.png" />
-            <div className="slide-block">
-              <IonText color="dark">
-                <h2>Всегда под рукой </h2>
-              </IonText>
-              <IonText> <sub>Каталог с мини-приложениями находится на центральной вкладке. Пользователи легко его найдут</sub></IonText>
-            </div>
-            <div className="slide-button">
-              <IonButton expand="full" onClick={handleButtonClick}>Что ещё?</IonButton>
-            </div>
+            <SlideContent
+                title={"Всегда под рукой "}
+                onClick={handleButtonClick}
+                description={
+                  "Каталог с мини-приложениями находится на центральной вкладке. Пользователи легко его найдут"
+                }
+                buttonTitle={"Что ещё?"}
+                imgSrc={"/assets/slide3.png"}
+            ></SlideContent>
           </IonSlide>
           <IonSlide>
-            <img src="/assets/slide4.png" />
-            <div className="slide-block">
-              <IonText color="dark">
-                <h2>Баннер с ваши предложением</h2>
-              </IonText>
-              <IonText> <sub>Уникальная скидка, спецпредложение или акция. Донесите ценное предложение до всех пользователей Aitu</sub></IonText>
-            </div>
-            <div className="slide-button">
-              <IonButton expand="full" onClick={handleButtonClick}>Далее</IonButton>
-            </div>
-          </IonSlide>
-          <IonSlide>
-            <img src="/assets/slide5.png" />
-            <div className="slide-block">
-              <IonText color="dark">
-                <h2>Коммуникация с пользователями</h2>
-              </IonText>
-              <IonText><sub>Отправляйте пуш-уведомления своим пользователям</sub></IonText>
-            </div>
-            <div className="slide-button">
-              <IonButton expand="full" onClick={handleButtonClick}>Отправить пуш-уведомление</IonButton>
-            </div>
+            <SlideContent
+                title={"Баннер с ваши предложением"}
+                onClick={handleButtonClick}
+                description={
+                  "         Уникальная скидка, спецпредложение или акция. Донесите ценное предложение до всех пользователей Aitu"
+                }
+                buttonTitle={"Далее"}
+                imgSrc={"/assets/slide4.png"}
+            ></SlideContent>
           </IonSlide>
         </IonSlides>
       </IonContent>
